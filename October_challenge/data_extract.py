94a4551eccb743a21a3b34d8ccfca1507eaa8368
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-10-20 16:01:23 +0300

    initial commit

---
+++ October_challenge/data_extract.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+import pandas as pd
+import openpyxl
+
+
+data = './October_challenge/Fin_consumer.xlsx'
+
+fact_data = pd.read_excel(data, sheet_name= 'Complaints (Fact)', header= 0)
+#print(fact_data.head())
+
+dim_data = pd.read_excel(data, sheet_name= 'Company', header= 0)
+
+#print(fact_data.head())
+#print(dim_data.head())
+
+
+#here we start transafroming out datasets
+
+
+""""
+We shall start with our fact table
+"""
+
+print(fact_data.describe())
+print(fact_data.info())
