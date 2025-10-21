673a0dd2af7ee9885278f11bb0c404e54fbab120
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-10-21 12:29:20 +0300

    data cleaning and transformation

--- October_challenge/data_extract.py
+++ October_challenge/data_extract.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import openpyxl
+import datetime
 
 
 data = './October_challenge/Fin_consumer.xlsx'
@@ -25,5 +26,22 @@ We shall start with our fact table
 print(fact_data.describe())
 print(fact_data.info())
 
-print(fact_data.columns)
-print(dim_data.columns)
+#print(fact_data.columns)
+#print(dim_data.columns)
+
+
+#print(fact_data['Date submitted'].head())
+
+#correcting date columns
+
+fact_data['Date submitted'] = pd.to_datetime(fact_data['Date submitted'], origin= '1899-12-30', unit= 'D')
+fact_data['Date received'] = pd.to_datetime(fact_data['Date received'], origin= '1899-12-30', unit= 'D')
+fact_data['Company_Response_Date'] = pd.to_datetime(fact_data['Company_Response_Date'], origin= '1899-12-30', unit= 'D')
+
+
+print(fact_data.head())
+
+
+
+
+
