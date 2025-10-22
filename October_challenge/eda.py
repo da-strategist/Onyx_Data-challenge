af0542192d572dcbeb3563cce3cfa87e94df92fd
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-10-22 14:51:04 +0300

    eda working on identifying complaint patterns

---
+++ October_challenge/eda.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import plotly
+
+
+
+#we shall start our eda by addressing the first objective
+
+"Identify complaint patterns across financial products, services, companies, and geographic regions."
+""
+"Questions to be answered"
+""
+"1. Total complaints recieved"
+"2. Complaint distribution across channels, most frequently used channels"
+"3. Complaint across companies, size, category etc"
+"4. Complaints across regions, identifying hotspots"
+"5. Complaints across products and sub products, identifying most common issues"
+"6. Response rate, overall and distribution across companies. most responsive and quickest in terms of issue resolution"
+"7a. Trends, yearly, monthly, weekly and daily distribution of complaints, peak complaints periods and low complaint periods"
+"7b Change in complaints over time i.e progress"
+""
+
+""
+""
